7ec3c1586d9f281637f7a800f2dfd960891ac30c
Wilton Souza <wiltonsouzafilho@yahoo.com.br>
2025-04-01 20:50:30 -0300

    feat: adding metrics-server chart

---
+++ terraform/metrics-server.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
+
+  timeout = 300
+
+  values = [file("${path.module}/charts/metrics-server/values.yaml")]
+}
